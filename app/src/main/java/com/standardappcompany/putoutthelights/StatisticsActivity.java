c60d9d4af93dbdb91fc8dbfb4a795f156329ae66
Steven Van Landuyt <contact@standardAppCompany.com>
2023-06-04 15:15:08 -0500

    Tried to get the back arrow in the stats screen working.  Reverted it..

--- app/src/main/java/com/standardappcompany/putoutthelights/StatisticsActivity.java
+++ app/src/main/java/com/standardappcompany/putoutthelights/StatisticsActivity.java
@@ -1,5 +1,10 @@
 package com.standardappcompany.putoutthelights;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StatisticsActivity extends AppCompatActivity {
